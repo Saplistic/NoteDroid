5eab20bf5a266d800da826345f3f6a664a7a6c0c
Mat. Devski Carlier <carliermateo@outlook.be>
2023-12-14 22:14:20 +0100

    Created Note creation functionality

--- app/src/main/java/be/ehb/notedroid/views/CreateFragment.java
+++ app/src/main/java/be/ehb/notedroid/views/CreateFragment.java
@@ -2,15 +2,29 @@ package be.ehb.notedroid.views;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import be.ehb.notedroid.NotesBase;
 import be.ehb.notedroid.R;
+import be.ehb.notedroid.models.Note;
+import be.ehb.notedroid.viewmodels.NotesViewModel;
 
 public class CreateFragment extends Fragment {
+
+    EditText title;
+    EditText content;
+    Button submitBtn;
 
     public CreateFragment() {
     }
@@ -29,5 +43,29 @@ public class CreateFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_create, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        NotesViewModel notesViewModel = new ViewModelProvider(getActivity()).get(NotesViewModel.class);
+
+        title = view.findViewById(R.id.et_note_create_title);
+        content = view.findViewById(R.id.et_note_create_content);
+        submitBtn = view.findViewById(R.id.btn_note_save);
+
+        submitBtn.setOnClickListener((View v) -> {
+            String titleTxt = title.getText().toString();
+            String contentTxt = content.getText().toString();
+
+            Note newNote = new Note(titleTxt, contentTxt);
+
+            NotesBase.getInstance().AddNote(newNote);
+
+            Toast.makeText(getContext(), "Created new note", Toast.LENGTH_SHORT).show();
+
+            Navigation.findNavController(view).navigateUp();
+        });
     }
 }
